f691e332d7d4032a4070c1701466cf5e5139968b
Kynan <k.song@blueyonder.co.uk>
2017-10-16 19:26:59 +0100

    Passed rock wins test.

--- models/game_logic.rb
+++ models/game_logic.rb
@@ -3,12 +3,18 @@ class Game
   def initialize(move1, move2)
     @move1 = move1
     @move2 = move2
+    @moves = [@move1, @move2]
   end
 
   def play
     if @move1 == @move2
       return "Draw"
+    elsif @move1 == 'rock' && @move2 == 'scissors'
+      return "#{@move1} beats #{@move2}"
+
     end
   end
+
+
 
 end
--- specs/game_logic_spec.rb
+++ specs/game_logic_spec.rb
@@ -5,10 +5,15 @@ class TestGame < Minitest::Test
 
   def setup
     @game = Game.new('rock', 'rock')
+    @game2 = Game.new('rock', 'scissors')
   end
 
   def test_draw()
     assert_equal("Draw", @game.play)
+  end
+
+  def test_rock_win()
+    assert_equal("rock beats scissors", @game2.play)
   end
 
 end
