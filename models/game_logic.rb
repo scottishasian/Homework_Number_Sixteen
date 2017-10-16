6b8a07ac90cd6dca908d57291f6ff7053c1c759a
Kynan <k.song@blueyonder.co.uk>
2017-10-16 19:35:56 +0100

    past test scissors win

--- models/game_logic.rb
+++ models/game_logic.rb
@@ -9,8 +9,11 @@ class Game
   def play
     if @move1 == @move2
       return "Draw"
-    elsif @move1 == 'rock' && @move2 == 'scissors'
-      return "#{@move1} beats #{@move2}"
+    elsif @move1 == 'rock' && @move2 == 'scissors' || @move2 == 'rock' && @move1 == 'scissors'
+      return "rock beats scissors"
+    elsif @move1 == 'scissors' && @move2 == 'paper' || @move2 == 'scissors' && @move1 == 'paper'
+      return "scissors beats paper"
+
 
     end
   end
--- specs/game_logic_spec.rb
+++ specs/game_logic_spec.rb
@@ -6,6 +6,9 @@ class TestGame < Minitest::Test
   def setup
     @game = Game.new('rock', 'rock')
     @game2 = Game.new('rock', 'scissors')
+    @game3 = Game.new('scissors', 'rock')
+    @game4 = Game.new('scissors', 'paper')
+    @game5 = Game.new('paper', 'scissors')
   end
 
   def test_draw()
@@ -14,6 +17,12 @@ class TestGame < Minitest::Test
 
   def test_rock_win()
     assert_equal("rock beats scissors", @game2.play)
+    assert_equal("rock beats scissors", @game3.play)
+  end
+
+  def test_scissor_win
+    assert_equal("scissors beats paper", @game4.play)
+    assert_equal("scissors beats paper", @game5.play)
   end
 
 end
