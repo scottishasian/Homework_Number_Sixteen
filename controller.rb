5dbf1af7b24028e06f1bb2dd8a112605d5e76b6d
Kynan <k.song@blueyonder.co.uk>
2017-10-16 20:27:43 +0100

    Added rules and home erb files.

--- controller.rb
+++ controller.rb
@@ -2,6 +2,18 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('./models/game_logic.rb')
 
+get '/' do
+  erb(:Home)
+end
+
+get '/rules' do
+  erb(:Rules)
+end
+
+get '/play' do
+  erb(:results)
+end
+
 get '/play/:move1/:move2' do
   play = Game.new(params[:move1], params[:move2])
   @game = play.play()
